43dddd4772a27806388b1545479971de55ba67a6
Dmitriy-17 <lopushansky.d@gmail.com>
2017-04-13 16:48:14 +0300

    add navbar

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 'use strict'
 
-var stomApp = angular.module("stomApp", []);
+var stomApp = angular.module("stomApp", ['ngRoute']);
 
+stomApp.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'homeCtrl'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'homeCtrl'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'views/contact.html',
+            controller: 'homeCtrl'
+        });
+});
